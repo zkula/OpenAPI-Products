8cfc581f03ae6eee39e165a3f3e02db35e20c77d
Zach Kula <zachkula@hotmail.com>
2023-02-20 18:22:07 -0500

    Create store new product dynamodb test

--- test/repositories/ProductsRepositoryDynamoDB.spec.ts
+++ test/repositories/ProductsRepositoryDynamoDB.spec.ts
@@ -1,11 +1,23 @@
+import { AttributeValue, GetItemCommand } from "@aws-sdk/client-dynamodb";
+import config from "config";
 import { NewProduct, Product } from "../../src/api/product/Product";
 import { ProductsRepositoryDynamoDB } from "../../src/api/product/ProductsRepositoryDynamoDB";
 import { createProduct } from "../helpers/createProduct";
+import { client, createProductsTable, deleteProductsTable } from "../helpers/productsTable";
 
 const getRepository = () => new ProductsRepositoryDynamoDB();
 
 describe("ProductsRepositoryDynamoDB", () => {
   describe("create", () => {
+    beforeAll(async () => {
+      await createProductsTable();
+    });
+
+    afterAll(async () => {
+      //Clean up
+      await deleteProductsTable();
+    });
+
     it("stores a NewProduct in the database and returns Product with newly generated id and actual createdAt date", async () => {
       const newProduct: NewProduct = createProduct({
         id: undefined,
@@ -19,10 +31,28 @@ describe("ProductsRepositoryDynamoDB", () => {
 
       const actual = await getRepository().create(newProduct);
 
+      //Retrieve database product entry
+      const output = await client.send(
+        new GetItemCommand({
+          TableName: config.get("dbTables.products.name"),
+          Key: {
+            ProductID: { S: actual.id },
+          },
+        }),
+      );
+
       expect(actual).toEqual(expectedProduct);
       expect(typeof actual.id).toBe("string");
       expect(actual.createdAt).toBeInstanceOf(Date);
       expect(new Date().getTime() - actual.createdAt.getTime()).toBeLessThan(1000);
+
+      expect(output.Item).not.toBeUndefined();
+      const item = output.Item as Record<string, AttributeValue>;
+      expect(item["ProductID"].S).toEqual(actual.id);
+      expect(item["Name"].S).toEqual(expectedProduct.name);
+      expect(item["Description"].S).toEqual(expectedProduct.description);
+      expect(item["Price"].N).toEqual(String(expectedProduct.price));
+      expect(item["CreatedAt"].N).toEqual(String(actual.createdAt.getTime()));
     });
   });
 });
