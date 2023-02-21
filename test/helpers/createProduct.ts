94c96aacb3089b904d24781f6d61c1fb6f475cc9
Zach Kula <zachkula@hotmail.com>
2023-02-21 07:52:32 -0500

    Create initial test cases for fetch product by id

--- test/helpers/createProduct.ts
+++ test/helpers/createProduct.ts
@@ -1,7 +1,7 @@
 import { v4 } from "uuid";
 import { Product } from "../../src/api/product/Product";
 
-export const createProduct = (product: Partial<Product>): Product => ({
+export const createProduct = (product: Partial<Product> = {}): Product => ({
   name: `product-name-${v4()}`,
   description: `product-description-${v4()}`,
   price: Math.random() * 50,
--- test/repositories/ProductsRepositoryDynamoDB.spec.ts
+++ test/repositories/ProductsRepositoryDynamoDB.spec.ts
@@ -1,5 +1,6 @@
-import { AttributeValue, GetItemCommand } from "@aws-sdk/client-dynamodb";
+import { AttributeValue, GetItemCommand, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import config from "config";
+import { v4 } from "uuid";
 import { NewProduct, Product } from "../../src/api/product/Product";
 import { ProductsRepositoryDynamoDB } from "../../src/api/product/ProductsRepositoryDynamoDB";
 import { createProduct } from "../helpers/createProduct";
@@ -8,16 +9,16 @@ import { client, createProductsTable, deleteProductsTable } from "../helpers/pro
 const getRepository = () => new ProductsRepositoryDynamoDB();
 
 describe("ProductsRepositoryDynamoDB", () => {
+  beforeAll(async () => {
+    await createProductsTable();
+  });
+
+  afterAll(async () => {
+    //Clean up
+    await deleteProductsTable();
+  });
+
   describe("create", () => {
-    beforeAll(async () => {
-      await createProductsTable();
-    });
-
-    afterAll(async () => {
-      //Clean up
-      await deleteProductsTable();
-    });
-
     it("stores a NewProduct in the database and returns Product with newly generated id and actual createdAt date", async () => {
       const newProduct: NewProduct = createProduct({
         id: undefined,
@@ -53,6 +54,46 @@ describe("ProductsRepositoryDynamoDB", () => {
       expect(item["Description"].S).toEqual(expectedProduct.description);
       expect(item["Price"].N).toEqual(String(expectedProduct.price));
       expect(item["CreatedAt"].N).toEqual(String(actual.createdAt.getTime()));
+    });
+  });
+
+  describe("fetchById", () => {
+    it("Returns undefined if the product with given id does not exist", async () => {
+      const id = v4();
+      const actual = await getRepository().fetchById(id);
+
+      expect(actual).toEqual(undefined);
+    });
+
+    it("Returns a product if product with given id exists in the database", async () => {
+      const product = createProduct();
+
+      const output = await client.send(
+        new PutItemCommand({
+          TableName: config.get("dbTables.products.name"),
+          Item: {
+            ProductID: { S: product.id },
+            Name: { S: product.name },
+            Description: { S: product.description },
+            Price: { N: String(product.price) },
+            CreatedAt: { N: product.createdAt.getTime().toString() },
+          },
+        }),
+      );
+
+      const actual = await getRepository().fetchById(product.id);
+
+      expect(actual).toEqual(product);
+      // expect(typeof actual.id).toBe("string");
+      // expect(actual.createdAt).toBeInstanceOf(Date);
+      // expect(new Date().getTime() - actual.createdAt.getTime()).toBeLessThan(1000);
+      // expect(output.Item).not.toBeUndefined();
+      // const item = output.Item as Record<string, AttributeValue>;
+      // expect(item["ProductID"].S).toEqual(actual.id);
+      // expect(item["Name"].S).toEqual(expectedProduct.name);
+      // expect(item["Description"].S).toEqual(expectedProduct.description);
+      // expect(item["Price"].N).toEqual(String(expectedProduct.price));
+      // expect(item["CreatedAt"].N).toEqual(String(actual.createdAt.getTime()));
     });
   });
 });
