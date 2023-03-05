8a59805b87ed1c077c5b30fcfa979107c9b8e077
Zach Kula <zachkula@hotmail.com>
2023-03-05 15:10:43 -0500

    Create ProductsController

---
+++ src/api/product/ProductsController.ts
@@ -0,0 +1,29 @@
+import { inject } from "inversify";
+import { Controller, Get, Route, Security, SuccessResponse } from "tsoa";
+import securities from "../auth/securities";
+import { provideSingleton } from "../../util/provideSingleton";
+import { Product } from "./Product";
+import { ProductsRepository } from "./ProductsRepository";
+import { ApiError } from "../ApiError";
+
+export type ProductsResponseBody = {
+  products: Product[];
+};
+
+@Route("products")
+@provideSingleton(ProductsController)
+export class ProductsController extends Controller {
+  constructor(@inject("ProductsRepository") private productsRepository: ProductsRepository) {
+    super();
+  }
+
+  @SuccessResponse(200)
+  @Security(securities.USER_AUTH)
+  @Get()
+  public async listProducts(): Promise<ProductsResponseBody> {
+    const products = await this.productsRepository.fetchAll();
+    return {
+      products,
+    };
+  }
+}
