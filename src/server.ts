dd62feeaebf43d9ba318b1a0204089c9da1b32a9
Zach Kula <zachkula@hotmail.com>
2023-03-07 21:40:59 -0500

    Update configs

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import config from "config";
+import { createProductsTableIfDoesNotExist } from "../test/helpers/productsTable";
 import { app } from "./app";
 
 const port = config.get("server.port");
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+  createProductsTableIfDoesNotExist();
+});
