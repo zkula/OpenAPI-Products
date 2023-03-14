c58939c16681e397924638a5a6fb2d5658e227aa
Zach Kula <zachkula@hotmail.com>
2023-03-14 18:35:20 -0400

    Add example values on Product interface

--- src/api/product/Product.ts
+++ src/api/product/Product.ts
@@ -1,8 +1,23 @@
 export interface Product {
+  /**
+   * @example "7b891531-8daa-476b-9b8e-7a9695127b0f"
+   */
   id: string;
+  /**
+   * @example "2023-03-08T02:51:32.749Z"
+   */
   createdAt: Date;
+  /**
+   * @example "iPhone 12"
+   */
   name: string;
+  /**
+   * @example "Brand new"
+   */
   description: string;
+  /**
+   * @example "1099.99"
+   */
   price: number;
 }
 
